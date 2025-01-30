86ac7f46e3a03bf931fcc526e78d6014850c6534
RafeeqNaj <rafeeq.n@amplicon.com>
2025-01-30 15:53:42 +0000

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('a[href^="#"]');
+    for (const link of links) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    }
+});
